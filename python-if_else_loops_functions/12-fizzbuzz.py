63c41fbdf525516542708139f17fe1148b55411b
Clement <9397@holbertonstudents.com>
2024-09-04 12:02:08 +0200

    Fixed typo O to 0

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,6 +8,6 @@ def fizzbuzz():
         if i % 5 == 0:
             print("Buzz", end='')
 
-        elif i % 3 == 0 and i % 5 == O:
+        elif i % 3 == 0 and i % 5 == 0:
             print("{}".format(i), end='')
         print(" ", end='')
