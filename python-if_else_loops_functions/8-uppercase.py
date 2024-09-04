fd24a5a89fb0260a200769aff3469440a9804535
Clement <9397@holbertonstudents.com>
2024-09-04 11:18:32 +0200

    Refactored to use one less variable and no else statement

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,6 @@
 def uppercase(str):
     for c in str:
         if ord(c) >= ord('a') and ord(c) <= ord('z'):
-            char_to_print = chr(ord(c) - 32)
-        else:
-            char_to_print = c
-        print("{}".format(char_to_print), end='')
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end='')
     print("")
