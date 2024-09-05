4233ae211dc3d7b994d9f4c9aaa70e72426ba78c
Clement <9397@holbertonstudents.com>
2024-09-05 19:30:14 +0200

    Fixed logic to display correct sentences depending on arg count, and added variable argc for simpler logic

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,10 +3,13 @@
 if __name__ == '__main__':
     import sys
 
-    if len(sys.argv) == 2:
-        print("{} argument".format(len(sys.argv) - 1))
+    argc = len(sys.argv)
+    if argc == 1:
+        print("{} arguments.".format(argc - 1))
+    elif argc == 2:
+        print("{} argument:".format(argc - 1))
     else:
-        print("{} arguments".format(len(sys.argv) - 1))
+        print("{} arguments:".format(argc - 1))
 
-    for i in range(1, len(sys.argv)):
+    for i in range(1, argc):
         print("{}: {}".format(i, sys.argv[i]))
