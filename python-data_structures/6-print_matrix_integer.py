04120a64c43d8c948fbe2d35a6940b65f7080218
Clement <9397@holbertonstudents.com>
2024-09-10 14:15:54 +0200

    Fixed logic and refactored for simpler code

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{}".format(matrix[i][j]), end ='')
-            if j < len(matrix[i]) - 1:
-                print(" ", end='')
+    for row in matrix:
+        for element in row:
+            print("{:d}".format(element), end =' ')
         print("\n")
