b5a0d197e57123948b3a22912cda90678d633b6e
Clement <9397@holbertonstudents.com>
2024-09-10 14:02:04 +0200

    Fixed wrong indentation

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,4 +6,14 @@ def print_matrix_integer(matrix=[[]]):
             print("{}".format(matrix[i][j]), end ='')
             if j < len(matrix[i]) - 1:
                 print(" ", end='')
-            print("\n")
+        print("\n")
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
