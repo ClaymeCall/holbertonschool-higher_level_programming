f87cf1fa9272cf4b7244bf5a597020197af09da7
Clement <9397@holbertonstudents.com>
2024-09-10 16:15:44 +0200

    Coding style fixes

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,9 +2,9 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    #Padding eventual missing elements with zeros and storing the results
+    # Padding eventual missing elements with zeros and storing the results
     a1, a2 = (tuple_a + (0, 0))[:2]
     b1, b2 = (tuple_b + (0, 0))[:2]
 
-    result = (a1 + b1, a2 + b2) 
+    result = (a1 + b1, a2 + b2)
     return (result)
