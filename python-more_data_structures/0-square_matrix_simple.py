d6dc46e614326a495586608687f3c6f846b55b5e
Clement <9397@holbertonstudents.com>
2024-09-10 18:01:25 +0200

    Reformatted to avoid using a variable

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    res_matrix = [[x**2 for x in row] for row in matrix]
-    return res_matrix
+    return [[x ** 2 for x in row] for row in matrix]
