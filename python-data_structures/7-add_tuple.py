608fa1d76e47a888bfb536759ebb4d9be1a0254b
Clement <9397@holbertonstudents.com>
2024-09-10 16:14:02 +0200

    Added a function that adds tuples

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    #Padding eventual missing elements with zeros and storing the results
+    a1, a2 = (tuple_a + (0, 0))[:2]
+    b1, b2 = (tuple_b + (0, 0))[:2]
+
+    result = (a1 + b1, a2 + b2) 
+    return (result)
