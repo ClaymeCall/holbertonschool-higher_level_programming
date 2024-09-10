77e2d0da447ff6188bc1bdd35c328e55fe3d598a
Clement <9397@holbertonstudents.com>
2024-09-10 16:47:05 +0200

    Fixed wrong handling of a full negative numbers list returning 0 instead of the max

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -5,8 +5,8 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
 
-    max = 0
-    for num in my_list:
+    max = my_list[0]
+    for num in my_list[1:]:
         if num > max:
             max = num
 
