c55a79886a55918a431cadf75e58639ce6c59c67
Clement <9397@holbertonstudents.com>
2024-09-10 16:23:47 +0200

    Added handling of empty sentence returning a None first char

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    return (len(sentence), sentence[0])
+    if sentence is '':
+        first_char = None
+    else:
+        first_char = sentence[0]
+
+    return (len(sentence), first_char)
