f748e192158fbdea14fb708c649dcd8a182a5d97
Clement <9397@holbertonstudents.com>
2024-09-11 09:32:36 +0200

    Modified so it adds the unique elements of both lists, instead of only those of set_1 not in set_2

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    return [x for x in set_1 if x not in set_2]
+    diff_list = [x for x in set_1 if x not in set_2]
+    diff_list += [x for x in set_2 if x not in set_1]
+    return diff_list
