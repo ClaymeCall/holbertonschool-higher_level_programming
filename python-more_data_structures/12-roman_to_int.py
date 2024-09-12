0a3e79441cd36464e70afd4dec60c08defc4678c
Clement <9397@holbertonstudents.com>
2024-09-12 12:12:44 +0200

    Added a function that converts roman numbers to integers

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not roman_string: #Checking for incorrect input roman string
+        return 0
+
+    # Reversing roman string to facilitate its processing
+    rom = list(reversed(roman_string))
+
+    conv = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    }
+
+    res = conv[rom[0]]  # Start with the first value in the reversed string
+
+    # Loop through the reversed Roman string starting from the second element
+    for i in range(1, len(rom)):
+        # If current numeral is less than the previous one, subtract its value
+        if conv[rom[i]] < conv[rom[i - 1]]:
+            res -= conv[rom[i]]
+        else:
+            # Otherwise, just add its value
+            res += conv[rom[i]]
+
+    return res
