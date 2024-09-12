ec8bf6d6d0a5a0905026ab4abc02b0166c60b50f
Clement <9397@holbertonstudents.com>
2024-09-12 14:42:55 +0200

    Changed pass statement to break statement

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end='')
             counter += 1
-        except Exception:
-            pass
+        except IndexError:
+            break
     print()
     return counter
