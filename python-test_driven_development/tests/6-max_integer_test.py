986e4bd03c48d0714591e28a4150fa2836e8f0b6
Clement Callejon <9397@holbertonstudents.com>
2024-09-13 21:49:00 +0200

    Patched import statement

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -2,7 +2,7 @@
 """Unittest for max_integer([..])
 """
 import unittest
-from 6-max_integer import max_integer
+max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     """Test case for max_integer function."""
