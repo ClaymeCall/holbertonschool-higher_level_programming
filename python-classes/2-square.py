c5402e1e868678f414f6b43a45bbb17156340841
Clement <9397@holbertonstudents.com>
2024-09-16 21:16:28 +0200

    Added a module that provides a square class, with type and value check for the size attribute

---
+++ python-classes/2-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+1-square.py
+
+This module provides a Square class.
+"""
+
+
+class Square:
+    """
+    Defines a Square object.
+
+    Attributes:
+        size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize the square with a certain size.
+
+        Args:
+            size (no type): The size of the square.
+        """
+        if type(size) is int:
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size >= 0:
+            self.__size = size
+        else:
+            raise ValueError("size must be >= 0")
