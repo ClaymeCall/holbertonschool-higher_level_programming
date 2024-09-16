e3c6d795efccdb9f6118f56c6175b4259c880289
Clement <9397@holbertonstudents.com>
2024-09-16 21:48:25 +0200

    Added a module that provides a square class with private size attribute and area instance method

---
+++ python-classes/3-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+1-square.py
+
+This module provides a Square class.
+"""
+
+
+class Square:
+    """
+    Defines a Square object.
+
+    Attributes:
+        size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize the square with a certain size.
+
+        Args:
+            size (no type): The size of the square.
+        """
+        if type(size) is int:
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size >= 0:
+            self.__size = size
+        else:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        Returns the area of the current square.
+
+        Args:
+        self (Square): Current instance of Square object.
+
+        Return:
+        Area of the square, as int.
+        """
+        return self.__size ** 2
