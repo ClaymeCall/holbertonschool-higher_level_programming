7597d34a87d31daba912af34dcf081f6289f8260
Clement <9397@holbertonstudents.com>
2024-09-24 16:49:36 +0200

    Added a method that raises an exception as its not yet implemented

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+This modules provides a BaseGeometry class.
+'''
+
+
+class BaseGeometry():
+    '''
+    Empty base geometry class.
+    '''
+    def area(self):
+        '''
+        Empty method that raises an exception as its not yet implemented.
+        '''
+        raise Exception("area() is not implemented")
+
