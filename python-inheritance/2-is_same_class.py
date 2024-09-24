1faa1b00f35f90004181860e50ffc52b915fe2a5
Clement <9397@holbertonstudents.com>
2024-09-24 14:52:09 +0200

    Added documentation

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+'''
+Defines a class checking function.
+'''
+
 
 def is_same_class(obj, a_class):
     '''
-    Tests wether an object is matching a certain class.
+    Tests wether an object is matching a certain class
 
-    Arguments:
+    Args:
     obj: the object to test.
     a_class: the class to test the objects type against.
+
+    Returns: True if types match, False otherwise
     '''
     res = type(obj) is a_class
     return res
