30909fb59a332f4b1be645228986e3127e32a184
Clement <9397@holbertonstudents.com>
2024-09-25 14:54:44 +0200

    Removed trailing white space

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -24,7 +24,7 @@ class BaseGeometry():
         Raises:
             TypeError: If value is not an integer.
             ValueError: If value is <= 0.
-        ''' 
+        '''
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
