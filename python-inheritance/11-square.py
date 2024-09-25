2d37e8a8c6fa01098c5441b814f0b00622b1f3e1
Clement <9397@holbertonstudents.com>
2024-09-25 23:17:16 +0200

    Added files for tasks 10 and 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+'''
+This module provides BaseGeometry, Rectangle and Square classes
+by order of inheritance.
+'''
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate a parameter as an integer.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        Returns:
+            value if no raises.
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+        return value
+
+
+class Rectangle(BaseGeometry):
+    '''
+    Represents a rectangle.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+    '''
+
+    def __init__(self, width, height):
+        """
+        Initialize the rectangle with a certain size.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        classname = self.__class__.__name__
+        str = "[{}] {}/{}".format(classname, self.__width, self.__height)
+        return str
+
+
+class Square(Rectangle):
+    '''
+    Represents a Square.
+
+    Attributes:
+        size (int): The size of the Square.
+    '''
+
+    def __init__(self, size):
+        """
+        Initialize the Square with a certain size.
+
+        Args:
+            size (int): The size of the Square.
+        """
+        self.__size = self.integer_validator("size", size)
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        classname = self.__class__.__name__
+        str = "[{}] {}/{}".format(classname, self.__size, self.__size)
+        return str
