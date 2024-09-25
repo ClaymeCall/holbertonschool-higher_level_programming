249d2f2f8d8a780718b1ef19a0ab3dd36d6bcf57
Clement <9397@holbertonstudents.com>
2024-09-25 09:25:12 +0200

    Added a method to BaseGeometry class that checks for TypeErrors and Value errors

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,3 +13,9 @@ class BaseGeometry():
         Empty method that raises an exception as its not yet implemented.
         '''
         raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
