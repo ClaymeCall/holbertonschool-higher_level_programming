2f80508502388671a5472b5d74a0bc7f3e7e9afd
Clement <9397@holbertonstudents.com>
2024-09-30 16:31:03 +0200

    Added documentation and removed useless variable

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+'''
+This modules provides a function that reads and prints a files content.
+'''
 
 def read_file(filename=""):
+    '''
+    Read a text file (UTF8) and print it to stdout.
+
+    Args:
+        filename (str): The name of the file to read. Defaults to "".
+
+    Returns:
+        None
+    '''
     with open(filename, 'r', encoding='utf-8') as file:
-        file_contents = file.read()
-        print(file_contents, end='')
+        print(file.read(), end='')
