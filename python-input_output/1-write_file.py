9188910f856d6d78610312c3fc10d8912dd0b2bf
Clement <9397@holbertonstudents.com>
2024-09-30 16:42:53 +0200

    Added a function that writes in a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+This modules provides a function that writes in a file.
+'''
+
+
+def write_file(filename="", text=""):
+    '''
+    Writes text in a text file (UTF8).
+
+    Args:
+        filename (str): The name of the file to write. Defaults to "".
+        text (str): The text to write. Defaults to "".
+
+    Returns:
+        The number of characters written
+    '''
+    with open(filename, 'w', encoding='utf-8') as file:
+        return(file.write(text))
