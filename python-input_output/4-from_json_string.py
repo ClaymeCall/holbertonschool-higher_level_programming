3a7fc806e7a6a3a16a25495ceff9121220f8d5ce
Clement <9397@holbertonstudents.com>
2024-10-01 16:36:34 +0200

    Added a function that decodes JSON to an object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+This module provides a function that decodes JSON to an object.
+'''
+import json
+
+
+def from_json_string(my_str):
+    '''
+    Returns an object representation from a JSON string.
+    Args:
+        my_str (string): The JSON string to process.
+
+    Returns:
+        An object containing the same data as the JSON.
+    '''
+    return json.dumps(my_str)
