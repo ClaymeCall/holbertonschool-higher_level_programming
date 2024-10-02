a0458787c1817c78a7e6dacf55eefb7397532766
Clement <9397@holbertonstudents.com>
2024-10-02 08:31:53 +0200

    Added a function that saves its arguments in a JSON file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+This module provides a function that writes all its arguments
+to a JSON file
+'''
+import json
+import sys
+from os import path
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+filename = 'add_item.json'
+
+if path.exists(filename):
+    object = load_from_json_file(filename)
+else:
+    object = []
+
+object.extend(sys.argv[1:])
+
+save_to_json_file(object, filename)
+
