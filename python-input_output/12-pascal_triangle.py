8856a94195c4945d252dcf15e10a747fa6de587a
Clement <9397@holbertonstudents.com>
2024-10-04 22:06:29 +0200

    Added a function that returns a representation of a Pascal Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    '''
+    Returns a Pascal triangle of height n in a 2D list.
+
+    Args:
+        n (int): Height of the triangle
+
+    Return:
+        triangle (List[List[int]]): Representation of a pascal triangle
+    '''
+    if n <= 0:
+        return []
+
+    # Initializing a 2D list with the tip of the triangle being 1
+    triangle = [[1]]
+
+    for i in range(1, n):
+        # Starting each row with 1
+        row = [1]
+
+        for j in range(1, i):
+            # Appending each number being the sum
+            # of its adjacent in the previous row
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+
+        # Ending each row with 1
+        row.append(1)
+
+        # Appending finished row to triangle
+        triangle.append(row)
+
+    return triangle
