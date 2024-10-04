62008c1d3abcb6369d2784096a90276df2bdf6d7
Clement <9397@holbertonstudents.com>
2024-10-04 09:52:10 +0200

    Added a module with class featuring basic serialization/deserialization using pickle

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+'''
+This module provides a function that pickles instances of data to a file.
+'''
+import pickle
+
+
+class CustomObject():
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+
+    def serialize(self, filename):
+        '''
+        Pickles the object in a file.
+
+        Args:
+            data (dict): The data to process.
+            filename (string): the file to write
+
+        Returns:
+            None
+        '''
+        with open(filename, 'wb', ) as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def deserialize(cls, filename):
+        '''
+        Returns an object from a pkl file.
+
+        Args:
+            filename (string): the name of the binary file to read
+
+        Returns:
+            deserialized object
+        '''
+        with open(filename, 'rb') as file:
+            return pickle.load(file)
