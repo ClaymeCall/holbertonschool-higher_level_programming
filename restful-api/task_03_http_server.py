86380fb41de5e187200c22669c4842654990c822
Clement <9397@holbertonstudents.com>
2024-10-14 13:39:07 +0200

    Added a simple HTTP request handler server

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+PORT = 8000
+
+
+class Server(BaseHTTPRequestHandler):
+    '''
+    This class includes basic components for a HTTP server
+    '''
+    def do_GET(self):
+        match self.path:
+            case "/":
+                self.send_response(200)
+                self.send_header("Content-type", "text/plain")
+                self.end_headers()
+                self.wfile.write(b'Hello, this is a simple API!')
+
+            case "/data":
+                data = {
+                    "name": "John",
+                    "age": 30,
+                    "city": "New York"
+                }
+                self.send_response(200)
+                self.send_header("Content-type", "text/plain")
+                self.end_headers()
+                self.wfile.write(json.dumps(data).encode())
+
+            case "/info":
+                info = {
+                    "version": "1.0",
+                    "description": "A simple API built with http.server"
+                }
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json.dumps(info).encode())
+
+            case "/status":
+                self.send_response(200)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(b"OK")
+
+            case _:
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(b"Endpoint not found")
+
+
+with HTTPServer(("", PORT), Server) as httpd:
+    print(f"Serving at port {PORT}")
+    httpd.serve_forever()
