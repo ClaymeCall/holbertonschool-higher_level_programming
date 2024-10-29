9569692d1fc287bb8a99063342fdfb77320ef9f4
Clement <9397@holbertonstudents.com>
2024-10-29 14:09:12 +0100

    Removed useless lines

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,5 +1,2 @@
-#!/bin/bash
--- 0-list_databases.sql
 -- Lists all databases in the server.
-
 SHOW DATABASES;
--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,2 @@
-#!/bin/bash
--- 1-create_database_if_missing.sql
 -- Creates a database if it doesnt exist.
-
 CREATE DATABASE if NOT EXISTS hbtn_0c_0;
