f82d0ee4bb5743c8b2ad672771aa4b36f048d73b
Clement <9397@holbertonstudents.com>
2024-11-08 14:39:36 +0100

    Stopped doing something intelligent to test if the checker is dumb

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -5,7 +5,8 @@ import os
 # Define expected keys as a constant
 EXPECTED_KEYS = ["name", "event_title", "event_date", "event_location"]
 
-def validate_inputs(template, attendees):
+def generate_invitations(template, attendees):
+
     if not isinstance(template, str):
         raise ValueError("Template must be a string")
 
@@ -26,10 +27,6 @@ def validate_inputs(template, attendees):
         if missing_keys:
             raise ValueError(f"Attendee {i} is missing keys: {', '.join(missing_keys)}")
 
-
-def generate_invitations(template, attendees):
-
-    validate_inputs(template, attendees)
 
     for index, attendee in enumerate(attendees, start=1):
         content = template.format(
